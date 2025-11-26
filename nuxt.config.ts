c0a27b4876eba77ab13d71b0c886e96061202dbd
David Mainoo <work.davidmainoo@gmail.com>
2025-11-26 14:30:00 +0000

    chore: formatting and set DM Sans as default font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,21 +4,25 @@ export default defineNuxtConfig({
     compatibilityDate: "2025-07-15",
     devtools: { enabled: true },
 
+    // imports: {
+    //     autoImport: false,
+    // },
+
     css: ["~/assets/css/main.css"],
     vite: {
         plugins: [tailwindcss()],
     },
 
     modules: [
-      "@nuxt/fonts",
-      "@nuxt/icon",
-      "@nuxt/image",
-      "@nuxt/scripts",
-      "@nuxt/test-utils",
-      "@nuxt/eslint",
-      "shadcn-nuxt",
-      "@nuxtjs/color-mode",
-      "nuxt-icons",
+        "@nuxt/fonts",
+        "@nuxt/icon",
+        "@nuxt/image",
+        "@nuxt/scripts",
+        "@nuxt/test-utils",
+        "@nuxt/eslint",
+        "shadcn-nuxt",
+        "@nuxtjs/color-mode",
+        "nuxt-icons",
     ],
 
     colorMode: {
